8213e8cbd88e259f3f58e235dcb2be54b32d708a
mintusarker <sarkermintu707@gmail.com>
2023-07-09 10:56:26 +0600

    booking

--- src/componant/Appointment/Modal/Modal.js
+++ src/componant/Appointment/Modal/Modal.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { format } from 'date-fns';
+import { AuthContext } from '../../context/AuthProvider';
+import { toast } from 'react-hot-toast';
 
 const Modal = ({ appointmentOptions, activeId, setShowModal, selectedDate }) => {
 
@@ -7,6 +9,8 @@ const Modal = ({ appointmentOptions, activeId, setShowModal, selectedDate }) => 
     const appointmentOption = appointmentOptions.find(opi => opi._id === activeId)
     const { name, price, slots, _id } = appointmentOption
 
+    const { user } = useContext(AuthContext);
+    // const {displayName, email} = user;
 
     const handleBooking = event => {
         event.preventDefault();
@@ -17,14 +21,28 @@ const Modal = ({ appointmentOptions, activeId, setShowModal, selectedDate }) => 
         const phone = form.phone.value;
         const booking = {
             appointmentDate: date,
-            // treatment: treatmentName,
+            treatment: appointmentOption?.name,
             patient: name,
             slot,
             email,
             phone,
-            price
-        }
+            price: appointmentOption?.price
+        };
+
         console.log(booking);
+        fetch('http://localhost:5000/bookings', {
+            method: 'POSt',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(booking)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                toast.success('Booking successfully done')
+                setShowModal(false)
+            })
     }
 
 
@@ -33,7 +51,7 @@ const Modal = ({ appointmentOptions, activeId, setShowModal, selectedDate }) => 
             <div className='max-w-[600px] absolute top-1/2 left-1/2 z-30 bg-zinc-600 rounded-md transform -translate-x-1/2 -translate-y-1/2 p-3 lg:w-[450px] md:w-[450px] sm:w-[450px] w-[400px]'>
                 <h3 className="text-3xl text-center font-bold text-white mt-3">{name}</h3>
                 <button onClick={() => setShowModal(false)} className="btn btn-sm btn-circle btn-ghost pb-4 bg-white hover:text-white text-xl absolute right-2 top-2">✕</button>
-                <form onSubmit={handleBooking} className="p-8">
+                <form onSubmit={handleBooking} className="p-4">
 
                     <input type="text" disabled value={date} className="input input-bordered w-full my-3" />
                     <select name="slot" className="select select-bordered w-full my-3">
@@ -44,11 +62,11 @@ const Modal = ({ appointmentOptions, activeId, setShowModal, selectedDate }) => 
                             >{slot}</option>)
                         }
                     </select>
-                    <input name="name" type="text" placeholder="Your Name" disabled className="input input-bordered w-full my-3" />
-                    <input name='email' type="email" placeholder="Email Address" disabled className="my-3 input input-bordered w-full" />
+                    <input name="name" type="text" defaultValue={user?.displayName} placeholder="Your Name" disabled className="input input-bordered w-full my-3" />
+                    <input name='email' type="email" defaultValue={user?.email} placeholder="Email Address" disabled className="my-3 input input-bordered w-full" />
                     <input name='phone' autoFocus type="text" placeholder="Phone Number" className="input input-bordered w-full my-3" required />
 
-                    <input type="submit" className='btn btn-accent w-full mt-3' value="Submit" />
+                    <input type="submit" className='btn btn-accent text-[15px]  w-full mt-3' value="Submit" />
 
                 </form>
             </div>
